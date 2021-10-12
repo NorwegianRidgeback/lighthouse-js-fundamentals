3338d0c52f5254c3b79d6bc42c4af62063a624c1
NorwegianRidgeback <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-10-12 09:32:14 -0400

    Function to find even numbers

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven = function (num){
+    return num % 2 === 0;
+}
+//const tenIsEven = isEven(10);
+//const elevenIsEven = isEven(11);
+
+console.log(tenIsEven(10));
+console.log(elevenIsEven(11));
+
